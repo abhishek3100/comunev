494873d57bff4bc1c63ee76e25d62e6e3d6943a5
abhishek <abhishekku3100.12@gmail.com>
2020-07-10 21:24:33 +0530

    code to connect

--- app.js
+++ app.js
@@ -1,11 +1,38 @@
 var express = require("express");
 var app = express();
 var port = 3000;
- 
-app.get("/", (req, res) => {
- res.send("Hello World");
+
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+
+var mongoose = require("mongoose");
+mongoose.Promise = global.Promise;
+mongoose.connect("mongodb://localhost:27017/comunev1");
+var nameSchema = new mongoose.Schema({
+    name: String,
+    email: String,
+    education: String,
+    position: String,
+    file: Buffer,
+    phone: Number
 });
- 
+var User = mongoose.model("User", nameSchema);
+app.post("/hiring", (req, res) =>{
+    var myData = new User(req.body);
+    myData.save().then(item =>{
+        res.sendFile(__dirname + "/signup_success.html");
+    })
+    .catch(err => {
+        res.status(400).send("Unable to save to Mongo");
+    });
+});
+
+app.get("/", (req, res) => {
+    //res.send("Hello World");
+    res.sendFile(__dirname + "/index.html");
+});
+
 app.listen(port, () => {
- console.log("Server listening on port " + port);
+    console.log("Server listening on port "+ port);
 });
